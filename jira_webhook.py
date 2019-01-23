105e36edefcebb1f8c5b6d74f531bde45f48aa3c
Jim Laney <jlaney@uw.edu>
2019-01-23 10:29:08 -0800

    adds webhook files

---
+++ jira_webhook.py
@@ -0,0 +1,28 @@
+from utils import get_jira_client
+import json
+import re
+
+ISSUE_RE = re.compile(r'([a-z]+-[0-9]+)', re.IGNORECASE)
+
+
+def main(*args, **kwargs):
+    branch = None
+    repository = None
+    commits = []
+
+    jira = get_jira_client()
+
+    for commit in commits:
+        message = commit['message']
+
+	for match in ISSUE_RE.findall(message):
+	    issue = jira.issue(match)
+
+	    jira.add_comment(issue, 'Commit on branch: {} ({})'.format(
+		branch, repository))
+	    issue.fields.labels.append('commit-{}'.format(branch))
+	    issue.update(fields={'labels': issue.fields.labels})
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='jira-webhook',
+    version='0.1',
+    packages=[],
+    author="UW-IT AXDD",
+    author_email="aca-it@uw.edu",
+    include_package_data=True,
+    install_requires=[
+        'jira',
+    ],
+    license='Apache License, Version 2.0',
+    description=('AXDD GitHub webhook for Jira'),
+    url='https://github.com/uw-it-aca/tech-inventory-update',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
