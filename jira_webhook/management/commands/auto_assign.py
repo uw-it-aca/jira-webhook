9654baca55fbe35961ef056f9f0496bb021e9853
Jim Laney <jlaney@uw.edu>
2021-04-08 08:19:22 -0700

    adds managenemt commands

---
+++ jira_webhook/management/commands/auto_assign.py
@@ -0,0 +1,46 @@
+# Copyright 2021 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from django.core.management.base import BaseCommand, CommandError
+from jira_webhook.dao.jira import JiraClient
+
+
+class Command(BaseCommand):
+    help = ('Assigns issues that have been fixed, but not assigned to'
+            'someone else')
+
+    def add_arguments(self, parser):
+        parser.add_argument('project')
+        parser.add_argument('assignee')
+
+    def handle(self, *args, **options):
+        project = options.get('project')
+        assignee = options.get('assignee')
+
+        jira = JiraClient()
+
+        issues = jira.search_issues((
+            'project = {} AND status = Resolved and resolution in (Fixed, '
+            'Completed) and (assignee != {} OR assignee IS EMPTY) and '
+            'updated < -15minute').format(project, assignee),
+            expand='changelog', maxResults=1000)
+
+        for issue in issues:
+            has_assignee_change = False
+            has_resolution = False
+            changelog = issue.changelog
+            changelog.histories.reverse()
+            resolution_date = None
+
+            for history in changelog.histories:
+                for item in history.items:
+                    if item.field == 'assignee':
+                        if not has_resolution:
+                            has_assignee_change = True
+                    elif item.field == 'status':
+                        has_resolution = True
+
+            if not has_assignee_change:
+                jira.add_comment(
+                    issue, 'Auto assigning issue to {}'.format(assignee))
+                jira.assign_issue(issue, assignee)
--- jira_webhook/views.py
+++ jira_webhook/views.py
@@ -4,7 +4,6 @@
 from django.conf import settings
 from django.views import View
 from django.http import HttpResponse
-from django.core.signing import BadSignature
 from jira.exceptions import JIRAError
 from jira_webhook.dao.jira import JiraClient
 import hmac
@@ -14,46 +13,42 @@ import json
 
 class APIView(View):
     def verify_signature(self, request):
-        body = request.body.encode('utf-8')
-        signature = request.META.get('HTTP_X_HUB_SIGNATURE', '')
-        token = getattr(settings, 'GITHUB_TOKEN', '')
+        h = hmac.new(getattr(settings, 'GITHUB_WEBHOOK_SECRET', ''),
+                     msg=request.body.encode('utf-8'),
+                     digestmod=hashlib.sha256)
 
-        h = hmac.new(token, body, hashlib.sha1)
-        digest = 'sha1={}'.format(h.hexdigest())
+        digest = 'sha256={}'.format(h.hexdigest())
+        signature = request.META.get('HTTP_X_HUB_SIGNATURE_256')
 
-        if digest != signature:
-            raise BadSignature()
+        return hmac.compare_digest(digest, signature)
 
     def post(self, request, *args, **kwargs):
+        # Verify message signature
+        if not self.verify_signature(request):
+            return HttpResponse('Invalid signature', status=403)
+
+        # Verify event type
+        if request.META.get('HTTP_X_GITHUB_EVENT', '') != 'push':
+            return HttpResponse(status=204)
+
         try:
-            self.verify_signature(request)
-        except BadSignature as ex:
-            return HttpResponse('{}'.format(ex), status=403)
+            data = json.loads(request.body)
+        except Exception as ex:
+            return HttpResponse('{}'.format(ex), status=400)
 
-        event_type = request.META.get('HTTP_X_GITHUB_EVENT', '')
+        client = JiraClient()
 
-        if event_type == 'push':
+        branch = data.get('ref')
+        repository = data.get('repository').get('full_name')
+        for commit in data.get('commits', []):
             try:
-                data = json.loads(request.body)
+                client.process_commit(commit, branch, repository)
+            except JIRAError as ex:
+                if ex.status_code != 404:
+                    return HttpResponse(
+                        json.dumps({'url': ex.url, 'error': ex.text}),
+                        status=ex.status_code)
             except Exception as ex:
-                return HttpResponse('{}'.format(ex), status=400)
-
-            client = JiraClient(
-                host=getattr(settings, 'JIRA_HOST'),
-                auth=(getattr(settings, 'JIRA_USER'),
-                      getattr(settings, 'JIRA_PASS')))
-
-            branch = data.get('ref')
-            repository = data.get('repository').get('full_name')
-            for commit in data.get('commits', []):
-                try:
-                    client.process_commit(commit, branch, repository)
-                except JIRAError as ex:
-                    if ex.status_code != 404:
-                        return HttpResponse(
-                            json.dumps({'url': ex.url, 'error': ex.text}),
-                            status=ex.status_code)
-                except Exception as ex:
-                    return HttpResponse('{}'.format(ex), status=403)
+                return HttpResponse('{}'.format(ex), status=403)
 
         return HttpResponse(status=204)
