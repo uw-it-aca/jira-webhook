e8d416acab56950a7e0ed7c7d7d1cbf4dfed2e9a
Jim Laney <jlaney@uw.edu>
2021-04-08 16:51:03 -0700

    patch resource method

--- jira_webhook/__init__.py
+++ jira_webhook/__init__.py
@@ -5,14 +5,16 @@ from jira.resources import Resource
 
 UW_JIRA_BASE_URL = '{server}/{rest_path}/{rest_api_version}/{path}'
 
-"""
-Patch the jira.resources.Resource class to force the UW Jira API url format.
-"""
-# def _get_uw_url(self, path):
-#    options = self._options.copy()
-#    options.update({'path': path})
-#    return UW_JIRA_BASE_URL.format(**options)
-# Resource._get_url = _get_uw_url
-#
 
-Resource.JIRA_BASE_URL = UW_JIRA_BASE_URL
+"""
+Patch the Resource._get_url method to force the UW Jira API url format.
+"""
+
+
+def _get_uw_url(self, path):
+    options = self._options.copy()
+    options.update({'path': path})
+    return UW_JIRA_BASE_URL.format(**options)
+
+
+Resource._get_url = _get_uw_url
