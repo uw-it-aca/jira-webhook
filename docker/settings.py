97a0f6e65da10f7c721ce3b11c149b272d029382
Jim Laney <jlaney@uw.edu>
2021-04-08 13:27:58 -0700

    drops sso workflow

--- docker/settings.py
+++ docker/settings.py
@@ -5,11 +5,10 @@ INSTALLED_APPS += [
     'jira_webhook.apps.JiraWebhookConfig',
 ]
 
-GITHUB_WEBHOOK_SECRET = bytes(os.getenv('GITHUB_WEBHOOK_SECRET', ''), encoding='utf8')
+GITHUB_WEBHOOK_SECRET = bytes(os.getenv('GITHUB_WEBHOOK_SECRET', ''),
+                              encoding='utf8')
 GITHUB_API_TOKEN = os.getenv('GITHUB_API_TOKEN')
 
 JIRA_HOST = os.getenv('JIRA_HOST')
 JIRA_USER = os.getenv('JIRA_USER')
 JIRA_PASS = os.getenv('JIRA_PASS')
-
-IDP_HOST = 'https://idp.u.washington.edu/'
